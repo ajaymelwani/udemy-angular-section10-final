c55e732d4dbf82ee8441a94b3a9f1380f637afd2
Ajay Melwani <ajaymelwani1996@yahoo.com>
2020-08-17 00:37:19 +0530

    117. Managing Recipes in a Recipe Service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,17 @@
+import { Recipe } from './recipe.model';
+
 
 export class RecipeService {
-	
+
+	private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  	];
+
+
+  	//study Javascript Reference and Primitive Datatypes. We only get access to the copy of recipes array via the empty slice() method
+  	getRecipes() {
+  		return this.recipes.slice();
+  	}
+
 }
